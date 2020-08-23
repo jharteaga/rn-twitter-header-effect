71270877cf049f7039675abfe7a32a5bf98df225
jharteaga <jh.arteaga09@gmail.com>
2020-08-23 00:13:16 -0700

    Add header, photo, and styling

--- App.js
+++ App.js
@@ -1,21 +1,60 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, Image } from 'react-native';
+
+HEADER_MAX_HEIGHT = 120;
+HEADER_MIN_HEIGHT = 70;
+PROFILE_IMAGE_MAX_HEIGHT = 80;
+PROFILE_IMAGE_MIN_HEIGHT = 40;
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.header}></View>
+      <ScrollView style={styles.avatar}>
+        <View style={styles.imageContainer}>
+          <Image source={require('./assets/me.jpg')} style={styles.image} />
+        </View>
+        <View>
+          <Text style={styles.name}>José Arteaga</Text>
+        </View>
+      </ScrollView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  avatar: {
+    flex: 1,
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  header: {
+    position: 'absolute',
+    top: 0,
+    right: 0,
+    left: 0,
+    backgroundColor: 'lightskyblue',
+    height: HEADER_MAX_HEIGHT,
+  },
+  image: {
+    flex: 1,
+    width: null,
+    height: null,
+  },
+  imageContainer: {
+    height: PROFILE_IMAGE_MAX_HEIGHT,
+    width: PROFILE_IMAGE_MAX_HEIGHT,
+    borderRadius: PROFILE_IMAGE_MAX_HEIGHT / 2,
+    borderColor: 'white',
+    borderWidth: 3,
+    overflow: 'hidden',
+    marginTop: HEADER_MAX_HEIGHT - PROFILE_IMAGE_MAX_HEIGHT / 2,
+    marginLeft: 10,
+  },
+  name: {
+    fontWeight: 'bold',
+    fontSize: 26,
+    paddingLeft: 10,
   },
 });
